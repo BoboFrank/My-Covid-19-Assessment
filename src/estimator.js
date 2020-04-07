9641f08ef5c47e020cbda63416899b9b2133cdb8
BoboFrank <anozie201040@gmail.com>
2020-04-07 19:38:54 +0100

    Changes made to estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,6 +1,26 @@
-const covid19ImpactEstimator = (data) => {
-    data;
-    return()
+const data = {
+    region: {
+        name: "Africa",
+        avgAge: 19.7,
+        avgDailyIncomeInUSD: 5,
+        avgDailyIncomePopulation: 0.71
+    },
+    periodType: "days",
+    timeToElapse: 58,
+    reportedCases: 674,
+    population: 66622705,
+    totalHospitalBeds: 1380614
 };
+
+const estimator = {
+    reportedCases: data.reportedCases,
+    currentlyInfected: reportedCases * 10,
+    scurrentlyInfected: reportedCases * 50,
+    infectionsByRequestedTime: currentlyInfected * 1024,
+    infectionsByRequestedTime: scurrentlyInfected * 1024,
+    
+};
+
+const covid19ImpactEstimator = (data) => data;
 
 export default covid19ImpactEstimator;
