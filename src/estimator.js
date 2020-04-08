b296b9657b6d664b92cbdb894710f89af5f59f23
BoboFrank <anozie201040@gmail.com>
2020-04-08 16:54:47 +0100

    Editing estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,11 +1,40 @@
 /* eslint-disable linebreak-style */
-import { data, impact, severeImpact } from './data';
-
-const estimatorData = {
-  data,
-  impact,
-  severeImpact
+const data = {
+  region: {
+    name: 'Africa',
+    avgAge: 19.7,
+    avgDailyIncomeInUSD: 5,
+    avgDailyIncomePopulation: 0.71
+  },
+  periodType: 'days',
+  timeToElapse: 58,
+  reportedCases: 674,
+  population: 66622705,
+  totalHospitalBeds: 1380614
 };
-const covid19ImpactEstimator = () => estimatorData;
+
+const impact = {
+  currentlyInfected: (data.reportedCases * 10),
+  infectionByRequestedTime: ((data.reportedCases * 10) * 1024),
+  casesByRequestedTime: (((data.reportedCases * 10) * 1024) * 0.15),
+  hospitalBedByRequestedTime: ((((data.reportedCases * 10) * 1024) * 0.15) * 0.35),
+  casesForICUByRequestedTime: (((data.reportedCases * 10) * 1024) * 0.05),
+  casesForVentilatorsByRequestedTime: (((data.reportedCases * 10) * 1024) * 0.02),
+  dollarsInFlight: (((((data.reportedCases * 10) * 1024) * 0.65) * 1.5) * 30)
+};
+
+const severeImpact = {
+  currentlyInfected: (data.reportedCases * 50),
+  infectionByRequestedTime: ((data.reportedCases * 10) * 1024),
+  casesByRequestedTime: (((data.reportedCases * 10) * 1024) * 0.15),
+  hospitalBedByRequestedTime: ((((data.reportedCases * 10) * 1024) * 0.15) * 0.35),
+  casesForICUByRequestedTime: (((data.reportedCases * 10) * 1024) * 0.05),
+  casesForVentilatorsByRequestedTime: (((data.reportedCases * 10) * 1024) * 0.02),
+  dollarsInFlight: (((((data.reportedCases * 10) * 1024) * 0.65) * 1.5) * 30)
+};
+
+const covid19ImpactEstimator = (estimatorData) => {
+  estimatorData;
+};
 
 export default covid19ImpactEstimator;
